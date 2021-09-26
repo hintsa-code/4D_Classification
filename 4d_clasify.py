b58e46b4ef5f52668a95cbf6d466b328e955701e
unknown <Hintsa Fisseha>
2021-09-26 16:16:46 +0900

    4d_classify part 1 done

--- 4d_clasify.py
+++ 4d_clasify.py
@@ -1,3 +1,4 @@
+import os
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
@@ -6,6 +7,7 @@ from keras.layers import Dense, Dropout
 from keras.models import Sequential
 from sklearn.preprocessing import LabelBinarizer
 import tensorflow as tf
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
 
 np.random.seed(1233231)
 x1 = np.random.random(5000)
@@ -76,6 +78,8 @@ t_train = target[0:4000]
 x_test = data[4000:]
 t_test = target[4000:]
 
+##################################################
+
 model1 = Sequential()
 model1.add(Dense(16, input_dim = 2, activation = 'sigmoid'))
 model1.add(Dense(16, activation = 'sigmoid'))
@@ -84,5 +88,47 @@ model1.add(Dense(4,activation = 'softmax'))
 model1.compile(loss = 'categorical_crossentropy', optimizer = 'adam', metrics = ['accuracy'])
 
 hist1 = model1.fit(x_train, t_train, epochs = 50, batch_size = 20)
+
+
+plt.figure(figsize = (15,5))
+plt.plot(hist1.history['loss'], 'y', label = 'loss')
+plt.plot(hist1.history['accuracy'], 'b', label = 'accuracy')
+plt.xlabel('epoch')
+plt.title('Sigmoid_model')
+plt.legend(loc = 'best')
+plt.show()
+
+model1_loss_and_metrics = model1.evaluate(x_test, t_test, batch_size = 20)
+print(model1_loss_and_metrics)
+
+#################################################
+
+model2 = Sequential()
+model2.add(Dense(16, input_dim = 2, activation = 'relu'))
+model2.add(Dense(16, activation = 'relu'))
+model2.add(Dense(4,activation = 'softmax'))
+
+model2.compile(loss = 'categorical_crossentropy', optimizer = 'adam', metrics = ['accuracy'])
+
+hist2 = model2.fit(x_train, t_train, epochs = 50, batch_size = 20)
+
+
+plt.figure(figsize = (15,5))
+plt.plot(hist2.history['loss'], 'y', label = 'loss')
+plt.plot(hist2.history['accuracy'], 'b', label = 'accuracy')
+plt.xlabel('epoch')
+plt.title('Sigmoid_model')
+plt.legend(loc = 'best')
+plt.show()
+
+model2_loss_and_metrics = model2.evaluate(x_test, t_test, batch_size = 20)
+print(model2_loss_and_metrics)
+
+predict1 = model1.predict(x_test)
+predict2 = model2.predict(x_test)
+
+for i in range(len(t_test)):
+    if np.argmax(t_test[i]) != np.argmax(predict2[i]):
+        print(format(x_test[i,0], "1.4f"), format(x_test[i,1], "1.4f"), np.argmax(t_test[i]), np.argmax(predict2[i]))
 
 
